ed3bc7651b0c9e227a685c1fa3c900f41d0f7392
Alina <linakuptsova92@gmail.com>
2019-12-04 13:11:26 +0300

    feat: add section 'goods' in index.html

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+gulp.task('hello', function (done) {
+  console.log('Привет, мир!');
+  done();
+});
+
+// Static server
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./src"
+    }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('minify-css', function () {
+  return gulp.src("./src/css/*.css")
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest("./src/css"));
+});
